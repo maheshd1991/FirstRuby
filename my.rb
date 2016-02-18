deb964f57b557cba2472899014f51fde82a42801
maheshd1991 <mahesh.d@siriinnovations.com>
2016-02-18 15:21:38 +0530

    First Ruby File

---
+++ my.rb
@@ -0,0 +1,24 @@
+class Myfirst
+   def paren
+      puts "this is parent class"
+   end 
+end
+class Myclass<Myfirst
+   def a 
+    puts "hellow world" 
+   end
+   def b(a,b)
+    c=a+b
+   puts c
+   end
+  def c
+     your_array = [[["Club three Team one", 7800], ["Club three Team two", 7801]], [], [["Club four Team one", 7807], ["Club four Team two", 7808]], []]
+      print your_array.each {|i| i.flatten!}
+    
+  end
+end
+ck=Myclass.new
+
+
+ck.c
+
